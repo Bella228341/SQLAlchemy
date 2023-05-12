8a024c178f2a92c76c3eb6781f530ca6842b4949
Bella228341 <119053670+Bella228341@users.noreply.github.com>
2023-05-12 17:07:00 +0300

    models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from app import db
+from flask_login import UserMixin
+
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Users('{self.username}', '{self.email}"
+
+
+class Roles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"Roles('{self.name}')"
